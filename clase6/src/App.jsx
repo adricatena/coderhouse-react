7ce01977c142904a23598403c273f53e33907620
Adriano <adricatena@gmail.com>
2023-07-27 21:07:01 -0300

    crear items con map

--- clase6/src/App.jsx
+++ clase6/src/App.jsx
@@ -1,18 +1,65 @@
-// import './App.css'
+/* const productos = [
+  {
+    id: "1",
+    name: "Pantalon",
+    description: "Pantalon de algodon negro",
+    stock: 10,
+  },
+  {
+    id: "2",
+    name: "Buzo",
+    description: "Buzo deportivo azul",
+    stock: 0,
+  },
+  {
+    id: "3",
+    name: "Campera",
+    description: "Campera de jean",
+    stock: 0,
+  },
+]; */
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Item from "./components/Item";
 
 function App() {
-  const [showItem, setShowItem] = useState(true);
+  const [productos, setProductos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const aux = [
+        {
+          id: "1",
+          name: "Pantalon",
+          description: "Pantalon de algodon negro",
+          stock: 10,
+        },
+        {
+          id: "2",
+          name: "Buzo",
+          description: "Buzo deportivo azul",
+          stock: 0,
+        },
+        {
+          id: "3",
+          name: "Campera",
+          description: "Campera de jean",
+          stock: 0,
+        },
+      ];
+      setProductos(aux);
+      setIsLoading(false);
+    }, 2000);
+  }, []);
 
   return (
     <>
-      App
-      {showItem ? <Item /> : "No va a mostrar Item"}
-      <button onClick={() => setShowItem(!showItem)}>
-        Montar/Desmontar Item
-      </button>
+      {isLoading && <h4>Cargando...</h4>}
+      {!isLoading &&
+        productos.map((producto) => (
+          <Item key={producto.id} producto={producto} />
+        ))}
     </>
   );
 }
--- clase6/src/components/Item.jsx
+++ clase6/src/components/Item.jsx
@@ -1,34 +1,14 @@
-import { useEffect, useState } from "react";
-
-function Item() {
-  const [contador, setContador] = useState(0);
-  const [contadorB, setContadorB] = useState(0);
-
-  useEffect(() => {
-    console.log("Esto se va a ejecutar solo cuando se monta el componente");
-
-    // funcion de "limpieza"
-    return () => {
-      console.log("Esto se ejecuta cuando se desmonta el componente ITEM");
-    };
-  }, []);
-
-  useEffect(() => {
-    console.log(
-      "Esto se va a ejecutar solo cuando se modifique el estado 'contador'"
-    );
-  }, [contador, contadorB]);
-
+function Item({ producto }) {
   return (
-    <>
-      Item
-      <button onClick={() => setContador(contador + 1)}>
-        Contador: {contador}
-      </button>
-      <button onClick={() => setContadorB(contadorB + 1)}>
-        Contador B: {contadorB}
-      </button>
-    </>
+    <article key={producto.id}>
+      <h3>{producto.name}</h3>
+      <h6>{producto.description}</h6>
+      {producto.stock > 0 ? (
+        <button>Agregar al carrito!</button>
+      ) : (
+        <span>No tenemos mas productos de este tipo</span>
+      )}
+    </article>
   );
 }
 
