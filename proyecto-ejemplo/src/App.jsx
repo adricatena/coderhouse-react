af46391154bc244ac27a8e91667fe1791487aec2
Adriano <adricatena@gmail.com>
2023-08-10 15:40:15 -0300

    agregar bootstrap

--- proyecto-ejemplo/src/App.jsx
+++ proyecto-ejemplo/src/App.jsx
@@ -1,14 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-// import "./App.css";
 import { Link } from "react-router-dom";
 
 const categorias = ["Pantalones", "Remeras", "Buzos"];
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <main>
       <nav>
--- proyecto-ejemplo/src/main.jsx
+++ proyecto-ejemplo/src/main.jsx
@@ -1,7 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-// import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cart from "./routes/Cart.jsx";
 import Item from "./routes/Item.jsx";
