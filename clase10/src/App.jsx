49fdec8910115cbc1b08b2c57cd8cf37ffe25b5c
Adriano <adricatena@gmail.com>
2023-08-10 20:07:46 -0300

    eventos basicos y prevenir default

--- clase10/src/App.jsx
+++ clase10/src/App.jsx
@@ -1,27 +1,65 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+const vocales = ["a", "e", "i", "o", "u"];
+
 function App() {
   const [count, setCount] = useState(0);
-  const [ancho, setAncho] = useState(0);
 
-  useEffect(() => {
-    const handleResize = (evento) => {
-      console.log({ evento });
-      setAncho(evento.target.innerWidth);
-    };
-    addEventListener("resize", handleResize);
+  const handleClick = (evento) => console.log(evento);
 
-    return () => {
-      removeEventListener("resize", handleResize);
-    };
-  }, []);
+  /* const handleChangeInput = (evento) => {
+    console.log(evento);
+  }; */
+
+  /* const handleSubmit = (evento) => {
+    evento.preventDefault();
+    console.log(evento);
+  }; */
+
+  const handleChange = (evento) => {
+    console.log("Evento onChange", evento);
+  };
+
+  const handleKeyDown = (evento) => {
+    const { key } = evento;
+
+    const esVocal = vocales.some((vocal) => key === vocal);
+    /* let esVocal = false;
+    switch (key) {
+      case "a":
+      case "e":
+      case "i":
+      case "o":
+      case "u":
+        esVocal = true;
+        break;
+    } */
+    if (esVocal) {
+      evento.preventDefault();
+    }
+
+    console.log("Evento onKeyDown", evento);
+    console.log({ esVocal });
+  };
 
   return (
     <>
-      <p>El ancho de la pantalla es: {ancho}</p>
+      <form>
+        <label>
+          Ingresa tu nombre:
+          <input
+            type="text"
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+          />
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
+      <button onClick={handleClick}>boton para ver eventos</button>
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
